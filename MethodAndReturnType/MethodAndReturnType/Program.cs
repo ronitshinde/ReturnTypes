8b4012a11054582e28ca71c50780ac06d4289416
ronitshinde <rony.shinde753@gmail.com>
2023-04-26 10:08:53 +0530

    [Ronit] Add : UC1-All the return types

---
+++ MethodAndReturnType/MethodAndReturnType/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndReturnType
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ReturnTypes types = new ReturnTypes();
+            types.MethodTypes(15);
+            Console.WriteLine(types.IntMethodType(4,5));
+            Console.WriteLine(types.DoubleMethodType(10,5));
+            Console.WriteLine(types.StringMethodType());
+            Console.WriteLine(types.BoolMethodType(5));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodAndReturnType/MethodAndReturnType/ReturnTypes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndReturnType
+{
+    public class ReturnTypes
+    {
+        //Void,Int,String,Bool,Double
+        //Void does not have return type.
+        public void MethodTypes()
+        {
+            Console.WriteLine("Its a void method");
+        }
+        public void MethodTypes(int one)
+        {
+            Console.WriteLine(one);
+        }
+        public int IntMethodType()
+        {
+            return 4 + 3;
+        }
+        public int IntMethodType(int one, int two)
+        {
+            int three = one + two;
+            return three;
+            // return 4 + 3;
+        }
+        public double DoubleMethodType(double four, double five)
+        {
+            double six = four - five;
+            return six;
+        }
+        public string StringMethodType()
+        {
+            return "BridgeLabz";
+        }
+        public bool BoolMethodType(int ten)
+        {
+            if (ten == 10)
+                return true;
+            else
+                return false;
+        }
+    }
+}
